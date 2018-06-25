bfb59edcad7a0bad54ab960d8edbe0d3f1c73faf
Dimitris Klouvas <dimitris@giaola.com>
2018-06-25 11:26:33 +0000

    fix: added movie and movies option models

--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,43 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    air_date =  models.DateTimeField(null=True, blank=True)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def likes(self):
+        return self.opinions.filter(opinion=OPINION_LIKE).count()
+    
+    @property
+    def hates(self):
+        return self.opinions.filter(opinion=OPINION_HATE).count()
+
+
+OPINION_LIKE = 'L'
+OPINION_HATE = 'H'
+
+class MovieOpinion(models.Model):
+    OPINION_CHOICES = (
+        (OPINION_LIKE, _('like')),
+        (OPINION_HATE, _('hate')),
+    ) 
+
+    user =  models.ForeignKey(settings.AUTH_USER_MODEL, related_name='opinions')
+    movie = models.ForeignKey('movies.Movie', related_name='opinions')
+    opinion = models.CharField(max_length=1, choices=OPINION_CHOICES, 
+                                null=True, blank=True)
+    
+    class Meta:
+        unique_together = ( 
+            ('user', 'movie'),
+        )
